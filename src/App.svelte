933ac4a728a9d1f7aa129d00267c1a2ebfcd7b6c
Echo545 <evan.er545@gmail.com>
2021-11-30 09:01:31 -0800

    Handles API unavailable and updated icons

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,14 @@
 <script>
   import { onMount } from "svelte";
   import ListEntry from "./ListEntry.svelte";
-  import Username from "./Username.svelte";
-
-  //   import Request from "./Request.svelte";
 
   const PAGE_COUNT = 25;
-  let tickets_url =
-    "https://zcccodingchallenge54.zendesk.com/api/v2/tickets?page[size]=" +
-    PAGE_COUNT;
+  let tickets_url = `https://zcccodingchallenge54.zendesk.com/api/v2/tickets?page[size]=${PAGE_COUNT}`;
 
-  export let name;
   export let key;
 
   let result = [];
+  let error = null;
   let tickets = null;
 
   var headers = new Headers();
@@ -30,11 +25,22 @@
       const response = await fetch(tickets_url, requestOptions);
       result = await response.json();
       tickets = result.tickets;
-    } catch (error) {
-      console.log(error);
+    } catch (e) {
+      error = e;
+      //   console.log(e);
     }
   });
 
+function listEntryPackage(ticket) {
+	return {
+		subject: ticket.subject,
+		requester_id: ticket.requester_id,
+		created_at: ticket.created_at,
+		priority: ticket.priority,
+		updated_at: ticket.updated_at,
+		status: ticket.status,
+	};
+  };
   //   function getPage(url, key) {
   //     let result = [];
 
@@ -69,96 +75,112 @@
       <div class="container-fluid">
         <div class="card shadow">
           <div class="card-header py-3">
-            <p class="text-primary m-0 fw-bold">All Tickets</p>
+            {#if !error}
+              <p class="text-primary m-0 fw-bold">All Tickets</p>
+            {:else}
+              <h3>😢</h3>
+            {/if}
           </div>
-          <div class="card-body">
-            <div
-              class="table-responsive table mt-2"
-              id="dataTable"
-              role="grid"
-              aria-describedby="dataTable_info"
-            >
-              <table class="table my-0" id="dataTable">
-                <thead>
-                  <tr>
-                    <th>Status</th>
-                    <th>Subject</th>
-                    <th>Requester</th>
-                    <th>Requested</th>
-                    <th>Priority</th>
-                    <th>Updated</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {#if tickets}
-                    {#each tickets as ticket}
-                      <ListEntry {...ticket} />
+          {#if !error}
+            <div class="card-body">
+              <div
+                class="table-responsive table mt-2"
+                id="dataTable"
+                role="grid"
+                aria-describedby="dataTable_info"
+              >
+                <table class="table my-0" id="dataTable">
+                  <thead>
+                    <tr>
+                      <th>Status</th>
+                      <th>Subject</th>
+                      <th>Requester</th>
+                      <th>Requested</th>
+                      <th>Priority</th>
+                      <th>Updated</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {#if tickets}
+                      {#each tickets as ticket}
+                        <ListEntry {...listEntryPackage(ticket)} />
+                      {:else}
+                        <p>loading</p>
+                      {/each}
                     {:else}
-                      <p>loading</p>
-                    {/each}
-                  {:else}
-                    API Unavailable
-                  {/if}
-                </tbody>
-                <tfoot>
-                  <tr>
-                    <td><strong>Status</strong></td>
-                    <td><strong>Subject</strong></td>
-                    <td><strong>Requester</strong></td>
-                    <td><strong>Requested</strong></td>
-                    <td><strong>Priority</strong></td>
-                    <td><strong>Updated</strong></td>
-                  </tr>
-                </tfoot>
-              </table>
-            </div>
-            <div class="row d-xl-flex justify-content-xl-center">
-              <div
-                class="col-xl-4 d-xl-flex justify-content-xl-end align-items-xl-center"
-              >
-                <button class="btn btn-primary" type="button">
-                  <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none">
-                    <path
-                      d="M15 19L8 12L15 5"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>Previous Page</button
+                      API Unavailable
+                    {/if}
+                  </tbody>
+                  <tfoot>
+                    <tr>
+                      <td><strong>Status</strong></td>
+                      <td><strong>Subject</strong></td>
+                      <td><strong>Requester</strong></td>
+                      <td><strong>Requested</strong></td>
+                      <td><strong>Priority</strong></td>
+                      <td><strong>Updated</strong></td>
+                    </tr>
+                  </tfoot>
+                </table>
+              </div>
+              <div class="row d-xl-flex justify-content-xl-center">
+                <div
+                  class="col-xl-4 d-xl-flex justify-content-xl-end align-items-xl-center"
                 >
-              </div>
-              <div
-                class="col-xl-2 d-xl-flex justify-content-xl-center align-items-xl-center"
-              >
-                <p class="lead text-secondary d-xl-flex">
-                  Displaying 1-25 of 100
-                </p>
-              </div>
-              <div
-                class="col-xl-4 d-xl-flex justify-content-xl-start align-items-xl-center"
-              >
-                <button class="btn btn-primary" type="button"
-                  >Next Page
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="1em"
-                    height="1em"
-                    viewBox="0 0 24 24"
-                    fill="none"
+                  <button class="btn btn-primary" type="button">
+                    <svg
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                    >
+                      <path
+                        d="M15 19L8 12L15 5"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>Previous Page</button
                   >
-                    <path
-                      d="M9 5L16 12L9 19"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
-                </button>
+                </div>
+                <div
+                  class="col-xl-2 d-xl-flex justify-content-xl-center align-items-xl-center"
+                >
+                  <p class="lead text-secondary d-xl-flex">
+                    Displaying 1-25 of 100
+                  </p>
+                </div>
+                <div
+                  class="col-xl-4 d-xl-flex justify-content-xl-start align-items-xl-center"
+                >
+                  <button class="btn btn-primary" type="button"
+                    >Next Page
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                    >
+                      <path
+                        d="M9 5L16 12L9 19"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
+          {:else}
+            <h4 id="error-message">
+              Sorry, the API is currently unavailable
+              <p>See the console for a detailed error message.</p>
+            </h4>
+          {/if}
         </div>
       </div>
     </div>
@@ -216,17 +238,10 @@
 
 <!-- </main> -->
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
   h1 {
     color: #ff3e00;
     text-transform: uppercase;
-    font-size: 4em;
+    /* font-size: 4em; */
     font-weight: 100;
   }
 
@@ -234,5 +249,11 @@
     main {
       max-width: none;
     }
+  }
+
+  #error-message {
+    margin-top: 5%;
+    padding-bottom: 5%;
+    text-align: center;
   }
 </style>
--- src/ListEntry.svelte
+++ src/ListEntry.svelte
@@ -1,38 +1,58 @@
 <script>
-    import OpenIcon from "./OpenIcon.svelte";
-    import Username from "./Username.svelte";
+  import IconOpen from "./IconOpen.svelte";
+  import IconPending from "./IconPending.svelte";
+  import IconSolved from "./IconSolved.svelte";
+  import IconClosed from "./IconClosed.svelte";
+  import IconNew from "./IconNew.svelte";
 
-    export let subject;
-    export let requester_id;
-    export let created_at;  //requested
-    export let priority;
-    export let updated_at;
-    export let status;
+  import Username from "./Username.svelte";
 
-    let created_date = new Date(created_at);
-    let updated_date = new Date(updated_at);
+  export let subject;
+  export let requester_id;
+  export let created_at;
+  export let priority;
+  export let updated_at;
+  export let status;
+
+  // Convert dates to readable format
+  let created_date = new Date(created_at);
+  let updated_date = new Date(updated_at);
 </script>
 
 <tr>
-    <td>
-
-        {#if status == "open"}
-            <OpenIcon/>
-        {/if}
-    </td>
-    <td>{subject}</td>
-    <td>
-        <Username requester_id={requester_id} />
-    </td>
-    <!-- <td>{created_at}</td> -->
-    <td>{created_date.toLocaleDateString()}</td>
-    <td>
-        {#if priority}
-            {priority}
-        {:else}
-            -
-        {/if}
-    </td>
-    <td>{created_date.toLocaleDateString()}</td>
+  <td>
+    <!-- Display the ticket's icon -->
+    {#if status == "open"}
+      <IconOpen />
+    {:else if status == "solved"}
+      <IconSolved />
+    {:else if status == "pending"}
+      <IconPending />
+    {:else if status == "new"}
+      <IconNew />
+    {:else}
+      <IconClosed />
+    {/if}
+  </td>
+  <td>{subject}</td>
+  <td>
+    <Username {requester_id} />
+  </td>
+  <td>
+    {created_date.toLocaleDateString()}
+  </td>
+  <td>
+    {#if priority}
+      {priority}
+    {:else}
+      -
+    {/if}
+  </td>
+  <td>{updated_date.toLocaleDateString()}</td>
 </tr>
 
+<style>
+  tr:hover {
+    background-color: rgba(173, 216, 230, 0.35);
+  }
+</style>
